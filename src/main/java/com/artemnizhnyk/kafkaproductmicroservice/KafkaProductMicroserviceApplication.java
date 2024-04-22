d869bb4e9353f453b8654db49c871ad1dd70f29b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-22 16:41:28 +0200

    base config for kafka producer

---
+++ src/main/java/com/artemnizhnyk/kafkaproductmicroservice/KafkaProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.kafkaproductmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProductMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProductMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemnizhnyk/kafkaproductmicroservice/KafkaProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.kafkaproductmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProductMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
