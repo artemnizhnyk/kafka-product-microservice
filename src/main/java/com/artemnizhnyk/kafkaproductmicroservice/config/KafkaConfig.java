e85d3a875598f264fa9c760bbe96e7040e4ec69c
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-27 12:16:56 +0200

    practice with Kafka Producer idempotence

--- src/main/java/com/artemnizhnyk/kafkaproductmicroservice/config/KafkaConfig.java
+++ src/main/java/com/artemnizhnyk/kafkaproductmicroservice/config/KafkaConfig.java
@@ -31,6 +31,10 @@ public class KafkaConfig {
     private String linger;
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String idempotence;
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private String maxInFlightRequests;
 
     Map<String, Object> producerConfigs() {
         Map<String, Object> config = new HashMap<>();
@@ -42,6 +46,8 @@ public class KafkaConfig {
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequests);
 
         return config;
     }
